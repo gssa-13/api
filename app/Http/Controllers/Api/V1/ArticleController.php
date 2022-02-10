cbbe739bab632fb8e6ab16153b26e10dd570e290
gssa-13 <gssa-13@protonmail.com>
2022-02-09 21:55:09 -0600

    Implemented route to get all articles

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\ArticleCollection;
 use App\Http\Resources\V1\ArticleResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+
+    public function index(): ArticleCollection
+    {
+        return ArticleCollection::make(Article::all());
+    }
+
     public function show(Article $article): ArticleResource
     {
         return ArticleResource::make($article);
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Api\V1\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('articles/{article}', [ArticleController::class, 'show'])->name('api.v1.articles.show');
+Route::get('articles', [ArticleController::class, 'index'])->name('api.v1.articles.index');
