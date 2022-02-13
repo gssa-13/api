8bdd112e3a98d1d7a4cf38935c05bc6e3e89132a
gssa-13 <gssa-13@protonmail.com>
2022-02-12 21:21:17 -0600

    Http request for tests are modified

---
+++ tests/MakesJsonApiRequests.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Testing\TestResponse;
+
+trait MakesJsonApiRequests
+{
+    /**
+     * Call the given URI with a JSON request.
+     *
+     * @param  string  $method
+     * @param  string  $uri
+     * @param  array  $data
+     * @param  array  $headers
+     * @return \Illuminate\Testing\TestResponse
+     */
+    public function json($method, $uri, array $data = [], array $headers = []): TestResponse
+    {
+        $headers['accept'] = 'application/vnd.api+json';
+
+        return parent::json($method, $uri, $data, $headers);
+    }
+
+    /**
+     * Visit the given URI with a POST request, expecting a JSON response.
+     *
+     * @param  string  $uri
+     * @param  array  $data
+     * @param  array  $headers
+     * @return \Illuminate\Testing\TestResponse
+     */
+    public function postJson($uri, array $data = [], array $headers = []): TestResponse
+    {
+        $headers['content-type'] = 'application/vnd.api+json';
+
+        return parent::postJson($uri, $data, $headers);
+    }
+
+    /**
+     * Visit the given URI with a PATCH request, expecting a JSON response.
+     *
+     * @param  string  $uri
+     * @param  array  $data
+     * @param  array  $headers
+     * @return \Illuminate\Testing\TestResponse
+     */
+    public function patchJson($uri, array $data = [], array $headers = []): TestResponse
+    {
+        return $this->json('PATCH', $uri, $data, $headers);
+    }
+}
